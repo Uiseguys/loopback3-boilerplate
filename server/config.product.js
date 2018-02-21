efa3c565cce08a33695ab32b69bb06f97fac61c1
ahadortiz <ahadortiz@outlook.com>
2018-02-21 11:52:46 -0300

    update config

---
+++ server/config.product.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  restApiRoot: '/api',
+  port: process.env.PORT || 3000,
+  remoting: {
+    context: false,
+    rest: {
+      handleErrors: false,
+      normalizeHttpPath: false,
+      xml: false,
+    },
+    json: {
+      strict: false,
+      limit: '100kb',
+    },
+    urlencoded: {
+      extended: true,
+      limit: '100kb',
+    },
+    cors: false,
+  },
+};
